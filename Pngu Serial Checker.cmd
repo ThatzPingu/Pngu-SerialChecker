75aa0d64396f87c17c3ce9edcad00a222bda3a6c
Glrdd Aye <138173113+ThatzPingu@users.noreply.github.com>
2025-01-08 18:35:04 +0100

    Pngu Serial Checker.cmd

--- Pngu Serial Checker.cmd
+++ Pngu Serial Checker.cmd
@@ -1,34 +1,49 @@
 @echo off
-mode con: cols=70 lines=45
-chcp 65001
 title Pngu Serial Checker
+chcp 65001
+color f0
+mode con: cols=90 lines=52
 :Main
-cls
 echo.
-echo               ╔════════════════════════════╗ 
-echo               ║        ╔═╗╔╗╔╔═╗╦ ╦        ║ 
-echo               ║        ╠═╝║║║║ ╦║ ║        ║
-echo               ║        ╩  ╝╚╝╚═╝╚═╝        ║      
-echo               ╚════════════════════════════╝      
+echo    [30m       ██████╗ ██╗███╗   ██╗ ██████╗ ██╗   ██╗         
+echo           ██╔══██╗██║████╗  ██║██╔════╝ ██║   ██║         
+echo           ██████╔╝██║██╔██╗ ██║██║  ███╗██║   ██║         
+echo           ██╔═══╝ ██║██║╚██╗██║██║   ██║██║   ██║         
+echo           ██║     ██║██║ ╚████║╚██████╔╝╚██████╔╝         
+echo           ╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝          
+echo.
 echo        ╔═════════════════════════════════════════╗
 echo        ║          [Pngu Serial Checker]          ║
-echo        ║      https://discord.gg/p2qhYvRrb8      ║
 echo        ╚═════════════════════════════════════════╝
 echo.
-echo [Bios Information]
+echo [32m[40mBios Information[0m
 wmic bios get serialnumber
-echo [System UUID]
-wmic csproduct get uuid
-echo [CPU Information]
-wmic cpu get serialnumber, processorid
-echo [Disk Drive Information]
-wmic diskdrive get serialnumber
-echo [Baseboard Information]
+::::::::::::::::::::::::::::::::::::::::::::::
+echo [32m[40m[Disk Drive Information][0m
+wmic diskdrive get model, serialnumber
+::::::::::::::::::::::::::::::::::::::::::::::
+echo [32m[40m[Motherboard Information][0m
 wmic baseboard get manufacturer, serialnumber
-echo [Network Adapter MAC Address]
+::::::::::::::::::::::::::::::::::::::::::::::
+echo [32m[40m[SMbios Information][0m
+wmic path win32_computersystemproduct get uuid
+::::::::::::::::::::::::::::::::::::::::::::::
+echo [32m[40m[GPU Information][0m
+wmic PATH Win32_VideoController GET Description,PNPDeviceID
+::::::::::::::::::::::::::::::::::::::::::::::
+echo [32m[40m[RAM Information][0m
+wmic memorychip get serialnumber
+::::::::::::::::::::::::::::::::::::::::::::::
+echo [32m[40m[System UUID][0m
+wmic csproduct get uuid
+::::::::::::::::::::::::::::::::::::::::::::::
+echo [32m[40m[CPU Information][0m
+wmic cpu get serialnumber, processorid
+::::::::::::::::::::::::::::::::::::::::::::::
+echo [32m[40m[MacAddress Information][0m
 wmic path Win32_NetworkAdapter where "PNPDeviceID like '%%PCI%%' AND NetConnectionStatus=2 AND AdapterTypeID='0'" get MacAddress
-echo.
-echo Press ENTER to refresh...
-pause >nul
+getmac
+echo.[107m
+PAUSE
 cls
 goto Main
